5e2401921c57ddc5a5ef687affd70ddc61720e91
Mansurkhoja <mansur.1999@bk.ru>
2021-06-29 15:34:36 +0500

    s

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 # git push -f git@github.com/Mansurkhoja/vue-hello.git master:gh-pages
-git push -f https://github.com/Mansurkhoja/vue-hello.git master:gh-pages
+git push -f https://github.com/Mansurkhoja/tommy-jeans.git master:gh-pages
 
 cd -
